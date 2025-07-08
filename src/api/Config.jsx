3f221a45458ca4412396a1d6d4bd0a5b48c71728
manish singh <pratapmanish63@gmail.com>
2025-07-09 00:07:34 +0530

    api address set to render url

--- src/api/Config.jsx
+++ src/api/Config.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://indievibe-backend-9e9e.onrender.com",
 });
 
 export default instance;
